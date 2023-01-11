a649f19249a5d495b1d60843964f8c723a0d432e
sharathnairv <sharath.vijayan@edstem.com>
2023-01-11 20:58:37 +0530

    .NET Training Day 3
    - OOPs concept high level
    - Interfaces
    - Polymorphism
    - Inheritance
    - Objects and instantiation
    - Overriding

---
+++ OOPsConcepts/Person.cs
@@ -0,0 +1,52 @@
+﻿// Encapsulation
+class Person
+{
+    // default constructor
+    // these are implicit in nature
+    public Person(string firstName, string lastName, int age, double salary) 
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        _salary= salary;
+    }
+
+    // Properties
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+
+    // Fields 
+    private double _salary;
+
+    //Methods
+    public void SetSalary(double salary)
+    {
+        _salary = salary;
+    }
+
+    public double GetSalary()
+    {
+        return _salary;
+    }
+
+    public string GetFullName()
+    {
+        return FirstName + " " +  LastName;
+    }
+
+    // an example of method overloading
+    public string GetFullName(string middleName) 
+    {
+        return FirstName + " " + middleName + " " + LastName;   
+    }
+}
+
+
+/*
+ * 
+ - Difference between fields and properties:
+    1. Attributes which are private are called as fields and which are public are called Properties.
+    2. Classes which only has getter and not setter or if the state of the class cannot be changed it is called as immutable class.
+
+ */
